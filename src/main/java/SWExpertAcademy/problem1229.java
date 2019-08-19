0f67e1e86f8881089819b218263cfa2c20c7bf83
DahunJeongINU <JungDH@inu.ac.kr>
2019-08-19 16:09:03 +0900

    study_20190819

---
+++ src/main/java/SWExpertAcademy/problem1229.java
@@ -0,0 +1,47 @@
+package SWExpertAcademy;
+
+import java.io.*;
+import java.util.*;
+
+public class problem1229 {
+
+    public static void main( String[] args ) throws Exception{
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        StringTokenizer st;
+        StringBuilder sb = new StringBuilder();
+        for(int tc = 1; tc<=10;tc++){
+            int n = Integer.parseInt(br.readLine());
+            ArrayList<Integer> arr = new ArrayList<>();
+            st = new StringTokenizer(br.readLine(), " ");
+            for(int i=0;i<n;i++){
+                arr.add(Integer.parseInt(st.nextToken()));
+            }
+            int operNum = Integer.parseInt(br.readLine());
+            st = new StringTokenizer(br.readLine(), " ");
+            while(st.hasMoreTokens()){
+                String operator = st.nextToken();
+                int operIndex = Integer.parseInt(st.nextToken());
+                int operLength = Integer.parseInt(st.nextToken());
+                if(operator.equals("I")) {
+                    for (int i = 0; i < operLength; i++) {
+                        arr.add(operIndex + i, Integer.parseInt(st.nextToken()));
+                    }
+                }
+
+                if(operator.equals("D")){
+                    for(int i=0;i<operLength;i++){
+                        arr.remove(operIndex);
+                    }
+                }
+            }
+            String temp ="";
+            for(int i=0;i<10;i++){
+                temp+=arr.get(i)+" ";
+            }
+            sb.append("#").append(tc).append(" ").append(temp).append("\n");
+        }
+
+        System.out.println(sb);
+        br.close();
+    }
+}
