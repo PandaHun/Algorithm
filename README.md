3b6dcda0fcc9b403ab3043b373c1aa0a3008d156
PandaHun <gnsgnsek@gmail.com>
2020-03-16 16:20:20 +0900

    Update BJ_다리 만들기

--- README.md
+++ README.md
@@ -44,6 +44,7 @@ Language: JAVA
 * 1987 - [알파벳](src/main/java/Baekjoon/problem1987.java)
 * 2003 - [수들의 합](src/main/java/Baekjoon/problem2003.java)
 * 2042 - [구간 합 구하기](src/main/java/Baekjoon/problem2042.java)
+* 2146 - [다리 만들기](src/main/java/Baekjoon/problem2146.java)
 * 2178 - [미로 탐색](src/main/java/Baekjoon/problem2178.java)
 * 2206 - [벽 부수고 이동하기](src/main/java/Baekjoon/problem2206.java)
 * 2309 - [일곱 난쟁이](src/main/java/Baekjoon/problem2309.java)
--- src/main/java/Baekjoon/problem2146.java
+++ src/main/java/Baekjoon/problem2146.java
@@ -1,115 +1,98 @@
 package Baekjoon;
-/*
- *  @Author: Pandahun
- *  @Content: 다리 만들기
- */
 
-import java.io.*;
-import java.util.*;
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.LinkedList;
+import java.util.Queue;
+import java.util.StringTokenizer;
 
 public class problem2146 {
 
-    private static int N;
-    private static int[][] map, nmap;
-    private static boolean[][] visited;
-    private static int[] dr = {-1,1,0,0};
-    private static int[] dc = {0,0,-1,1};
-    private static int answer = Integer.MAX_VALUE;
+    static int N;
+    static int[][] island;
+    static boolean[][] visited;
+    static int[] dr = {-1, 1, 0, 0};
+    static int[] dc = {0, 0, -1, 1};
 
-    public static void main( String[] args ) throws IOException, NumberFormatException{
+    public static void main(String[] args) throws IOException, NumberFormatException {
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
         StringTokenizer st;
         N = Integer.parseInt(br.readLine());
-        map = new int[N][N];
-        nmap = new int[N][N];
+        island = new int[N][N];
         visited = new boolean[N][N];
-        for(int i = 0 ; i< N; i++){
+        for (int r = 0; r < N; r++) {
             st = new StringTokenizer(br.readLine(), " ");
-            for(int j = 0 ; j < N; j++){
-                map[i][j] = Integer.parseInt(st.nextToken());
+            for (int c = 0; c < N; c++) {
+                island[r][c] = Integer.parseInt(st.nextToken());
             }
         }
-
-
-        int cnt = 1;
-        for(int i = 0 ; i < N ; i++){
-            for(int j = 0 ; j < N ; j++){
-                if(map[i][j] == 1) {
-                    cnt++;
-                    makeIsland(i, j, cnt);
+        int islandNumber = 1;
+        for (int r = 0; r < N; r++) {
+            for (int c = 0; c < N; c++) {
+                if (visited[r][c] || island[r][c] == 0) {
+                    continue;
                 }
+                setIsland(r, c, islandNumber);
+                islandNumber++;
             }
         }
-        set();
-        for(int i = 0 ; i <N;i++){
-            for(int j = 0 ; j<N;j++){
-                if(map[i][j]!=0) {
-                    BFS(i, j);
-                    set();
-                }
-            }
+        int answer = Integer.MAX_VALUE;
+        for (int i = 1; i < islandNumber; i++) {
+            answer = Math.min(answer, getBridgeLength(i));
         }
-
         System.out.println(answer);
     }
-    static void BFS(int r, int c){
-        Queue<Island> islands = new LinkedList<>();
-        islands.add(new Island(r,c,0));
 
-        while(!islands.isEmpty()){
-            Island now = islands.poll();
-
-            for(int k = 0 ; k < 4; k ++){
-                int nr = now.r + dr[k];
-                int nc = now.c + dc[k];
-                int nlength = now.length+1;
-
-                if(nr < 0 || nc < 0 || nr >= N || nc >= N) continue;
-
-                if(nmap[nr][nc] == nmap[r][c]) continue;
-
-                if(nmap[nr][nc] == -1) continue;
-
-                if(nmap[nr][nc] !=0){
-                    answer = answer < now.length ? answer : nlength-1;
-                    return;
+    static int getBridgeLength(int number) {
+        Queue<int[]> queue = new LinkedList<>();
+        int[][] dist = new int[N][N];
+        for (int r = 0; r < N; r++) {
+            for (int c = 0; c < N; c++) {
+                if (island[r][c] == number) {
+                    dist[r][c] = 0;
+                    queue.add(new int[]{r, c});
+                } else if (island[r][c] == 0) {
+                    dist[r][c] = -1;
+                } else {
+                    dist[r][c] = -2;
                 }
-
-                nmap[nr][nc] =  -1;
-                islands.add(new Island(nr, nc, nlength));
             }
         }
-
-    }
-
-
-    static void set(){
-        for(int i = 0 ; i < N;i++){
-            nmap[i] = Arrays.copyOf(map[i], N);
+        int min = Integer.MAX_VALUE;
+        while (!queue.isEmpty()) {
+            int[] now = queue.poll();
+            int r = now[0];
+            int c = now[1];
+            for (int k = 0; k < 4; k++) {
+                int nr = r + dr[k];
+                int nc = c + dc[k];
+                if (nr < 0 || nc < 0 || nr >= N || nc >= N) {
+                    continue;
+                }
+                if (dist[nr][nc] == -2) {
+                    min = Math.min(min, dist[r][c]);
+                    continue;
+                } else if (dist[nr][nc] == -1) {
+                    queue.add(new int[]{nr,nc});
+                    dist[nr][nc] = dist[r][c] + 1;
+                }
+            }
         }
+        return min;
     }
 
-    static void makeIsland(int r, int c, int label){
-        for(int k = 0 ; k < 4; k++){
-            int nr = r +dr[k];
-            int nc = c +dc[k];
+    static void setIsland(int r, int c, int idx) {
+        visited[r][c] = true;
+        island[r][c] = idx;
+        for (int k = 0; k < 4; k++) {
+            int nr = r + dr[k];
+            int nc = c + dc[k];
 
-            if(nr < 0 || nc < 0 || nr >=N || nc>=N) continue;
-
-            if(map[nr][nc] == label || map[nr][nc] == 0) continue;
-
-            map[nr][nc] = label;
-            makeIsland(nr, nc, label);
+            if (nr < 0 || nc < 0 || nr >= N || nc >= N || visited[nr][nc] || island[nr][nc] == 0) {
+                continue;
+            }
+            setIsland(nr, nc, idx);
         }
-    }
-}
-class Island{
-    int r;
-    int c;
-    int length;
-    public Island(int r, int c, int length){
-        this.r = r;
-        this.c = c;
-        this.length = length;
     }
 }
